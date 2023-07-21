6ecb99a6004a9ecae7f0c1ba8405b84da02b80e3
Maximiliano <Elgrungemaxi1986@gmail.com>
2023-07-21 03:37:49 +0200

    Modifique head

--- database/config/config.js
+++ database/config/config.js
@@ -1,17 +1,21 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
-    username: "root",
-    password: "root",
-    database: "notepad_db",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database:  process.env.DB_DATABASE,
+    host:  process.env.DB_HOST,
     dialect: "mysql",
+    port :  process.env.DB_PORT
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+   username: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    host:  process.env.MYSQL_ADDON_HOST,
+    dialect:"mysql",
+    port :  process.env.MYSQL_ADDON_PORT
   },
   production: {
     username: "root",
