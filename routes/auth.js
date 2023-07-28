663e9e6776872b3cdbe64f1fb0172334002d13ed
Maximiliano <Elgrungemaxi1986@gmail.com>
2023-07-28 04:54:36 +0200

    agregue login con google

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const passport = require('passport');
 const {loginGoogle} = require("../controllers/authController")
 const db = require("../database/models")
+
 passport.serializeUser((user, done)=> done(null,user))
 passport.deserializeUser((user, done)=> done(null,user))
+
 /* AUTH*/
 router
       .get("/login/google", passport.authenticate("google"))
